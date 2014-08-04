061dcc8a7422f0a3a71b2610a90e9bda31e353fb
Jason Stallings <jason@stallin.gs>
2014-08-04 08:27:38 -0500

    Fixed bad merge.

--- feedhelper.php
+++ feedhelper.php
@@ -9,16 +9,7 @@
  * License: A "Slug" license name e.g. GPL2
  */
 
-
-
-
-<<<<<<< HEAD
-	$plugins=array();
-	function getPlugins()
-=======
-	//Going to switch to use WordPress function get_plugins(); 
-	private function getPlugins()
->>>>>>> FETCH_HEAD
+ 	function getPlugins()
 	{
 	  	$all_plugins = get_plugins();
 		$plugins=array_keys($all_plugins);
@@ -92,14 +83,10 @@ function feedhelper_settings_page()
 <p>
 	Press start to identify newlines at the end of PHP files. <br>
 	<?php
-<<<<<<< HEAD
 
   loopThroughFiles();
-  //var_dump($all_plugins);
-=======
-	$all_plugins = get_plugins();
-	print_r( $all_plugins, true ) 
->>>>>>> FETCH_HEAD
+
+
 	?>
 </p>
 <input type="button" name="feedhelper_go" id="feedhelper_go" class="button button-primary" value="Start">
