ad48d1fe8912820fa2b2360059f20b6d6d29eefd
Jason Stallings <jason@stallin.gs>
2014-08-04 08:25:33 -0500

    Basic functionality implemented. Next step, Ajax!

--- feedhelper.php
+++ feedhelper.php
@@ -11,53 +11,50 @@
 
 
 
-class FeedHelper
-{
-	private $plugins=array();
 
-	//Going to switch to use WordPress function get_plugins();
-	private function getPlugins()
+	$plugins=array();
+	function getPlugins()
 	{
-		foreach(glob('./*', GLOB_ONLYDIR) as $dir) 
-		{
-			$dir = str_replace('./', '', $dir);
-			array_push($this->$plugins,$dir);
-		}
+	  	$all_plugins = get_plugins();
+		$plugins=array_keys($all_plugins);
 	}
-	
-	public function loopThroughFiles()
+
+	function loopThroughFiles()
 	{
+	  	$all_plugins = get_plugins();
+		$plugins=array_keys($all_plugins);
+
+	  	$plugin_dir = ABSPATH . 'wp-content/plugins/';
 		foreach($plugins as $plugin)
-		{
-			$dir = new DirectoryIterator(__DIR__ . "/" . $plugin . "/");
-			foreach ($dir as $fileinfo) 
+		{ 
+		  	$plugin_a=explode("/",$plugin);
+		  	$plugin=$plugin_a[0];
+			$dir = new RecursiveDirectoryIterator($plugin_dir . $plugin . "/");
+			foreach (new RecursiveIteratorIterator($dir) as $filename => $file) 
 			{
-				if (strpos($fileinfo->getFilename(),".php")) 
+
+   				if (strpos($filename,".php")) 
 				{
-					if (findNewline($fileinfo->getpathName()))
+					if (findNewline($file->getpathName()))
 					{
-						echo "Found newline in" . $fileinfo->getpathName() . ".\n";
+					  echo "Found newline in" . $file->getpathName() . "<br>";
 					}	
 				}
-			}
+			}	
 		}	
 	}
-	
-	protected function findNewline($pluginfile)
-	{
-		$file=file_get_contents($pluginfile);
-		$lines = preg_split("(\\n|\\r)", $file, NULL, PREG_SPLIT_DELIM_CAPTURE);
 
-		if ($lines[count($lines)-1]=="")
+	function findNewline($pluginfile)
+	{
+		$data = file($pluginfile);
+		$line = $data[count($data)-1];
+	  
+		if ($line=="\n" || $line=="\n\r")
 		{
-			return true;
+  			return true;
 		}
 		return false;
 	}
-
-}
-
-
 
 // create custom plugin settings menu
 add_action('admin_menu', 'feedhelper_create_menu');
@@ -88,12 +85,11 @@ function feedhelper_settings_page()
 <div class="wrap">
 <h2>Feed Helper</h2>
 <p>
-	Press start to identify newlines at the end of PHP files. 
+	Press start to identify newlines at the end of PHP files. <br>
 	<?php
-	/*
-	$all_plugins = get_plugins();
-	print_r( $all_plugins, true ) 
-	*/
+
+  loopThroughFiles();
+  //var_dump($all_plugins);
 	?>
 </p>
 <input type="button" name="feedhelper_go" id="feedhelper_go" class="button button-primary" value="Start">
